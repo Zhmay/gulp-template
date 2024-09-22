185bb6da31207539deca05f4e6a03927dfb8501a
bogdan <bogdanzhmay@gmail.com>
2024-09-22 14:42:09 +0300

    split styles to diff files & updated reload for html

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -4,15 +4,19 @@ const { styles } = require('./sass');
 const { scripts } = require('./scripts');
 const { html } = require('./html');
 
+function reload(done) {
+    browserSync.reload();
+    done();
+}
+
 function serve() {
     browserSync.init({
         server: './build'
     });
 
-    gulp.watch('src/style/**/*.scss', gulp.series(styles));
-    gulp.watch('src/js/**/*.js', gulp.series(scripts));
-    gulp.watch('src/template/**/*.html', gulp.series(html));
-    gulp.watch('build/*.html').on('change', browserSync.reload);
+    gulp.watch('src/style/**/*.scss', gulp.series(styles, reload));
+    gulp.watch('src/js/**/*.js', gulp.series(scripts, reload));
+    gulp.watch('src/template/**/*.html', gulp.series(html, reload));  
 }
 
 exports.serve = serve;
